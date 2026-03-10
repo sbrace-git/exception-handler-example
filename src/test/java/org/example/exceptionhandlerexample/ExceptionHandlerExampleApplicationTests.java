97e5678b7f338ffe035b8875f7e63cd118a2cccd
sbrace <sbrace0822@gmail.com>
2026-03-10 23:31:30 +0800

    init

---
+++ src/test/java/org/example/exceptionhandlerexample/ExceptionHandlerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exceptionhandlerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
